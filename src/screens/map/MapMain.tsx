08ce525a19d9f90d0d99f1eae13bbf042f26647c
Odetola Azeez Opeyemi <yemiotola@gmail.com>
2023-05-09 12:33:01 +0100

    android fixup

--- src/screens/map/MapMain.tsx
+++ src/screens/map/MapMain.tsx
@@ -1,10 +1,177 @@
-import React from 'react'
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useRef, useEffect } from 'react'
+import { Platform, View, PermissionsAndroid, Linking, Alert, ToastAndroid } from "react-native";
 import MapView from "react-native-maps";
+import Geolocation, { GeoPosition } from 'react-native-geolocation-service';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
 import mapStyles from './mapStyles'
 
 
 export default function MapMain() {
+  const [locationPerm, setLocationPerm] = useState(false);
+  const [modal, showModal] = useState(false);
+  const [modalMode, showModalMode] = useState('');
+  const [forceLocation, setForceLocation] = useState(true);
+  const [highAccuracy, setHighAccuracy] = useState(true);
+  const [locationDialog, setLocationDialog] = useState(true);
+  const [significantChanges, setSignificantChanges] = useState(false);
+  const [observing, setObserving] = useState(false);
+  const [foregroundService, setForegroundService] = useState(false);
+  const [useLocationManager, setUseLocationManager] = useState(false);
+  const [location, setLocation] = useState<GeoPosition | null>(null);
+
+  const mapRef: any = useRef();
+  const interRef: any = useRef();
+  const myMarker: any = useRef();
+  const watchId = useRef<number | null>(null);
+
+  useEffect(() => {
+    return () => {
+      // stopLocationUpdates();
+    };
+  }, []);
+
+  const hasPermissionIOS = async () => {
+    const openSetting = () => {
+      Linking.openSettings().catch(() => {
+        Alert.alert('Unable to open settings');
+      });
+    };
+    const status = await Geolocation.requestAuthorization('whenInUse');
+
+    if (status === 'granted') {
+      return true;
+    }
+
+    if (status === 'denied') {
+      Alert.alert('Location permission denied');
+    }
+
+    if (status === 'disabled') {
+      Alert.alert(
+        `Turn on Location Services to allow us to determine your location.`,
+        '',
+        [
+          { text: 'Go to Settings', onPress: openSetting },
+          { text: "Don't Use Location", onPress: () => { } },
+        ],
+      );
+    }
+    return false;
+  };
+
+  const hasLocationPermission = async () => {
+    if (Platform.OS === 'ios') {
+      const hasPermission = await hasPermissionIOS();
+      return hasPermission;
+    }
+
+    if (Platform.OS === 'android' && Platform.Version < 23) {
+      return true;
+    }
+
+    const hasPermission = await PermissionsAndroid.check(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+
+    if (hasPermission) {
+      return true;
+    }
+
+    const status = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+
+    if (status === PermissionsAndroid.RESULTS.GRANTED) {
+      return true;
+    }
+
+    if (status === PermissionsAndroid.RESULTS.DENIED) {
+      ToastAndroid.show(
+        'Location permission denied by user.',
+        ToastAndroid.LONG,
+      );
+    } else if (status === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN) {
+      ToastAndroid.show(
+        'Location permission revoked by user.',
+        ToastAndroid.LONG,
+      );
+    }
+
+    return false;
+  };
+
+  const getLocation = async () => {
+    const hasPermission = await hasLocationPermission();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    Geolocation.getCurrentPosition(
+      position => {
+        setLocation(position);
+        console.log(position);
+      },
+      error => {
+        Alert.alert(`Code ${error.code}`, error.message);
+        setLocation(null);
+        console.log(error);
+      },
+      {
+        accuracy: {
+          android: 'high',
+          ios: 'best',
+        },
+        enableHighAccuracy: highAccuracy,
+        timeout: 15000,
+        maximumAge: 10000,
+        distanceFilter: 0,
+        forceRequestLocation: forceLocation,
+        forceLocationManager: useLocationManager,
+        showLocationDialog: locationDialog,
+      },
+    );
+  };
+
+  const getLocationUpdates = async () => {
+    const hasPermission = await hasLocationPermission();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    // if (Platform.OS === 'android' && foregroundService) {
+    //   await startForegroundService();
+    // }
+
+    setObserving(true);
+
+    watchId.current = Geolocation.watchPosition(
+      position => {
+        setLocation(position);
+        console.log(position);
+      },
+      error => {
+        setLocation(null);
+        console.log(error);
+      },
+      {
+        accuracy: {
+          android: 'high',
+          ios: 'best',
+        },
+        enableHighAccuracy: highAccuracy,
+        distanceFilter: 0,
+        interval: 5000,
+        fastestInterval: 2000,
+        forceRequestLocation: forceLocation,
+        forceLocationManager: useLocationManager,
+        showLocationDialog: locationDialog,
+        useSignificantChanges: significantChanges,
+      },
+    );
+  };
+
   return (
     <View style={mapStyles.container}>
       {/*Render our MapView*/}
